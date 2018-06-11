c389c6bc6b4262a4dce0754748e932ecf506d97a
Virgile Tellier <37550192+virgile-tellier@users.noreply.github.com>
2018-06-11 17:54:18 +0200

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,26 +1,31 @@
 import sys
 import re
+import os
+import datetime as dt
+
 
 class File_Reader():
-	"""Classe de lecture de fichiers text simples (txt, csv, tsv...).
-	Prend en compte les paramètres suivants:
-		-file_name: chemin du fichier à lire
-		-sep: séparateur de champ, aucun par defaut. En cas de separateur, chaque ligne sera une liste python avec un element pour chaque champ
-		-suppress_newlines: si on veut enlever les \n à la fin de chaque ligne, vrai par defaut
-		-skiplines: nombre de lignes à ignorer en début de fichier, 0 par defaut
-		-strip_chars_pattern: expression reguliere pour enlever certains caracteres ou expressions (ex: expace en debut de ligne), auncune par defaut
-		-encoding: l'encodage du fichier, par defaut utf8
+	"""Wrapper class for simple standard file reader (txt, csv, tsv...) with in-built generator.
+	
+	:param file_name: path to file
+	:param sep: field seperator, none by default.
+	:param suppress_newlines: Deletes \n at the end of every line, True by default
+	:param skiplines: number of lines to skip, 0 by default
+	:param strip_chars_pattern: regular expression for pattern deleting, none by default
+	:param encoding: file encoding, utf8 by default
 
-	Utilisation:
-		#Initialiser avec le chemin du fichier et les parametres voulus
-		my_file = File_Reader("chemin/vers/lefichier/")
-		#lecture ligne par ligne
-		for line in my_file.iter():
-			do_something(line)
-		#OU recuperation de chaque ligne dans une liste
-		all_lines = my_file.readlines()
-		#Une fois le fichier parcouru, il faut reutiliser cette classe si l'on veut relire le fichier.
-		"""
+	:example:
+
+	#Init
+	my_file = File_Reader("path/to/file")
+	#Read line by line
+	for line in my_file.iter():
+		do_something(line)
+
+	my_file2 = File_Reader("path/to/file")
+	#Get all lines in a python list
+	all_lines = my_file2.readlines()
+	"""
 	def __init__(self, file_name, sep = "", suppress_newlines = True, skiplines = 0, strip_chars_pattern = "", encoding = ""):
 		self.file_name = file_name
 		self.sep = sep
@@ -71,11 +76,10 @@ class File_Reader():
 class Task_Follower():
 	"""Mini barre de progression en pourcentage pour les executions linéaire longues.
 
-	Utilisation:
-		#initialiser avec le nombre d'opérations
-		t = Task_Follower(count)
-		#incrémenter l'avencement
-		t.step()"""
+	#initialiser avec le nombre d'opérations
+	t = Task_Follower(count)
+	#incrémenter l'avencement
+	t.step()"""
 	def __init__(self, taskcount, message = "Completion: "):
 		self.taskcount = taskcount
 		self.done = 0
@@ -100,3 +104,164 @@ class Task_Follower():
 def head(l, start = 0, stop = 5):
 	"""Equivalent de l'outil linux head."""
 	print(l[start:stop])
+
+
+class File_Maker():
+	'''Wrapper class for simple file writting with version control. Auto-renaming of files to keep old files.
+	The last file will be tagged .latest (by default) et previous versions with a number (.1, .2, .3, etc).
+
+	:param path: path to save file WITHOUT extension and NO tags. Supply the extension in parameters.
+	:param data_stream: If not empty, will be written to the file with the save() method.
+	:param format: define the field separator for save() method. By default "". Other supported formats are csv, tsv et "".
+	:param extension: extension of save file. If none given, it will be infered with the path name or the format.
+	:param encoding: file encoding.
+	:param latest_string: Last version annotation tag. ".latest" by default.
+	:param olddata_dir: If given, path to directory where current ".latest" version will be moved. Number tags
+	will be added as needed.
+
+	:example:
+
+	from utils import File_Maker as FM
+
+	data = [["This", "is"], "my", 'data']
+	
+	# Init
+	# Moving and renaming old files is only done when save() or get_filepointer() are called.
+	save_file = FM("../test", data_stream = data, extension = ".txt", olddata_dir = "OLD_DATA/") 
+
+	# Writting data in ../test.latest.txt
+	save_file.save()
+
+	# Get the file pointer for further operations.	
+	with save_file.get_filepointer() as fp:
+		fp.write("adzaf")
+		fp.write("sth")
+		fp.write("rthter")
+		fp.write("rthet")
+		fp.close()
+	# File pointer disapears when close() is called.
+
+	'''
+	def __init__(self, path, data_stream = "", format = "tsv", extension = "", encoding = "utf-8",
+	latest_string = ".latest", olddata_dir = ""):
+	
+		self.path = path
+		
+		self.file_name = self.get_filename()
+
+		self.extension = extension
+		if not extension:
+			self.extension = self.get_extension()
+			if not self.extension:
+				self.extension = "."+format
+
+		self.set_savedir()
+		self.olddata_dir = olddata_dir
+
+		# self.replace_old = replace_old
+		# self.version_control = version_control
+
+		self.mode = "w"
+		self.encoding = encoding
+
+		if self.mode is not 'a' and self.mode is not 'w':
+			print("Warning, file oppening mode is not supported.")
+
+		
+		self.fp = None
+		self.data_stream = data_stream
+
+		self.format = format
+
+		self.format_dict = {
+		"tsv":'\t',
+		"csv":';',
+		"":""
+		}
+
+		self.sep = self.format_dict[format]
+		self.latest_string = latest_string
+
+
+	def get_filename(self):
+		name = (self.path.split("/")[-1])
+		if '.' in name:
+			name = ".".join(name.split('.')[0:-1])
+		return name
+
+	def get_extension(self):
+		ext = self.path
+		if ".." in ext:
+			ext = self.path.split("..")[-1]
+
+		if '.' in ext:
+			ext = ext.split(".")[-1]
+			return "."+ext
+		return ""
+
+	def set_datastream(self, data_stream):
+		self.data_stream = data_stream
+
+	def set_savedir(self):
+		sd_path = self.path.split("/")[0:-1]
+		if len(sd_path) is not 0:
+			os.chdir("/".join(sd_path))
+
+	def get_filepointer(self):
+		if self.fp:
+			return self.fp
+
+		save_name = self.file_name+self.latest_string+self.extension
+
+		if not self.olddata_dir:
+			files = [f for f in os.listdir() if os.path.isfile(f)]
+
+			if save_name in files:
+				i = 1
+				rename = save_name
+				while rename in files:
+					rename = self.file_name+"."+str(i)+self.extension
+					i+=1
+				os.rename(save_name, rename)
+
+		else:
+			dest_files = [f for f in os.listdir(self.olddata_dir) if os.path.isfile(os.path.join(self.olddata_dir,f))]
+			local_files = [f for f in os.listdir() if os.path.isfile(f)]	
+
+			if save_name in local_files:
+					i = 1
+					rename = self.file_name+"."+str(i)+self.extension
+					while rename in dest_files:
+						rename = self.file_name+"."+str(i)+self.extension
+						i+=1
+					new_path = os.path.join(self.olddata_dir,rename)
+					os.rename(save_name, new_path)
+
+		self.fp = open(save_name, self.mode, encoding = self.encoding)
+		return self.fp
+
+
+	def save(self):
+
+		if not self.fp:
+			self.fp = self.get_filepointer()
+
+		if self.data_stream and self.fp:
+			for i in self.data_stream:
+				if type(i) is not str:
+					self.fp.write(self.sep.join(i)+'\n')
+				else:
+					self.fp.write(i+'\n')
+
+		else:
+			sys.stdout.write("Missing data_stream or no file to write to.\n")
+			sys.stdout.flush()
+		
+
+	def close(self):
+		if self.fp:
+			self.fp.close()
+			self.fp = None
+		else:
+			sys.stdout.write("Can't close undefined file pointer.\n")
+			sys.stdout.flush()
